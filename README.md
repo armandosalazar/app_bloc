7b613d54270120b92a6558e02f458247fdc5269e
Armando Salazar <armandosalazar351@gmail.com>
2023-03-28 14:17:16 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# App BLoC
+## App create by Armando Salazar
+
+This is app create with pattern BLoC with flutter
---
+++ android/app/src/main/kotlin/me/armandosalazar/app_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.armandosalazar.app_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
